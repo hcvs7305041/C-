53bb9076239ead9bd613ab4eed399dd996275225
hcvs7305041 <48470597+hcvs7305041@users.noreply.github.com>
2019-06-07 14:15:01 +0800

    Update 排序.c

--- 排序.c
+++ 排序.c
@@ -1,6 +1,6 @@
 void sorting (int*,int);
 
-void sorting (int *x,int n){
+void sorting (int *x,int n){ //大到小
 	int a,b,c;
 	for(a=0;a<n;a++){
 		for(b=0;b<n;b++){
@@ -12,3 +12,18 @@ void sorting (int *x,int n){
 		}
 	}
 }
+
+/*
+void sorting (int *x,int n){ //小到大
+	int a,b,c;
+	for(a=0;a<n;a++){
+		for(b=0;b<n;b++){
+			if(x[b]<x[a]){
+				c=x[b];
+				x[b]=x[a];
+				x[a]=c;
+			}
+		}
+	}
+}
+*/
