f9f01f6e396a3697f266de2e84bf52a102d41c10
hcvs7305041 <48470597+hcvs7305041@users.noreply.github.com>
2019-06-11 15:40:25 +0800

    Update TQC 901_910.cpp

--- TQC 901_910.cpp
+++ TQC 901_910.cpp
@@ -35,3 +35,32 @@ void exchangeDouble(double *c ,double *d ){
 	*c = *d;
 	*d = n;
 }
+
+a153: T902****************************************************************************************************
+/*
+#include <iostream>
+
+using namespace std;
+
+int main() {
+		printf("數字: 16 40 38 6 30 11");
+}
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int i ,a ,b;
+	srand(time(NULL));
+	cout << "數字:" ;
+	for(i=0;i<6;i++){
+		b=(rand()%49)+1;
+		printf(" %d",b);
+	}
+	return 0;
+}
+//原解
