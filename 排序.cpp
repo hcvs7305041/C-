956b11af0bd140f17231d46e70fa3613ccea252f
hcvs7305041 <48470597+hcvs7305041@users.noreply.github.com>
2019-07-30 13:50:53 +0800

    Update 排序.cpp

--- 排序.cpp
+++ 排序.cpp
@@ -1,6 +1,6 @@
 void sorting (int*,int);
 
-void sorting (int *x,int n){ //小到大大到小
+void sorting (int *x,int n){ //小到大
 	int a,b,c;
 	for(a=0;a<n;a++){
 		for(b=0;b<n;b++){
@@ -16,8 +16,8 @@ void sorting (int *x,int n){ //小到大大到小
 /*
 void sorting (int *x,int n){ //大到小
 	int a,b,c;
-	for(a=0;a<n-1;a++){
-		for(b=0;b<n-1;b++){
+	for(a=0;a<n;a++){
+		for(b=0;b<n;b++){
 			if(x[b]<x[a]){
 				c=x[b];
 				x[b]=x[a];
