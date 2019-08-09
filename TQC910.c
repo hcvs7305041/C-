afe014231fb9e3d9ed388696085a4e1490532597
hcvs7305041 <48470597+hcvs7305041@users.noreply.github.com>
2019-08-09 09:31:07 +0800

    Update TQC910.c

--- TQC910.c
+++ TQC910.c
@@ -1,22 +1,47 @@
-//CPA09-1.c
-#include <stdio.h>
-#include <stdlib.h>
 
-void exchange(int*,int*);
- 
-int main() {
-	int a, b;
-	printf("請輸入兩個數字(數字跟數字中間請以空白相隔)");
-	 scanf("%d%d", &a, &b);
-	printf("a:%d b:%d\n", a, b);
-	exchange(&a,&b);
-	printf("a:%d b:%d\n", a, b);	
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(){    
+ 	struct node{
+  		char name[10];
+  		int score;
+ 		struct node* next;
+ 	};
+ 	struct node *a,*b,*c,*current;
+ 	a=malloc(sizeof(struct node));//配置動態記憶體大小方式
+ 	printf("請輸入第一位學生姓名: ");
+ 	 scanf("%s",a->name);
+ 	printf("分數: ");
+ 	 scanf("%d",&a->score );
+ 	a->next=NULL;
+ 	
+ 	b=malloc(sizeof(struct node));
+ 	printf("請輸入第二位學生姓名: ");
+ 	 scanf("%s",b->name);
+ 	printf("分數: ");
+ 	 scanf("%d",&b->score );
+	a->next=b;
+	b->next=NULL;
+	
+	c=malloc(sizeof(struct node));
+	printf("請輸入第三位學生姓名: ");
+	 scanf("%s",c->name);
+	printf("分數: ");
+	 scanf("%d",&c->score );
+	b->next=c;
+	c->next=NULL;
+ /*----------------------下面面-----------------------------*/
+	printf("\n輸出...\n");
+	current = a;
+	while(current!=NULL){
+		printf("學生: %s\n",(*current).name);
+		printf("分數: %d\n\n",(*current).score);
+		current=(*current).next;//每顯示完畢後一次，將指標指給下一個
+	}
+	free(a);//釋放記憶體
+	free(b);
+	free(c);
+
 	return 0;
-}
-
-exchange(int* a,int* b){
-	int r;
-	 r=*a;
-	 *a=*b;
-	 *b=r;
 }
