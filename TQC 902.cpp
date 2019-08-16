d99d8bafe90b4fb4f9db49a6e86b42383dada08c
hcvs7305041 <48470597+hcvs7305041@users.noreply.github.com>
2019-08-16 09:27:19 +0800

    Update TQC 902.cpp

--- TQC 902.cpp
+++ TQC 902.cpp
@@ -8,21 +8,21 @@ int main() {
 		printf("數字: 16 40 38 6 30 11");
 }
 */
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <iostream>
 
-using namespace std;
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h> 
 
 int main() {
-	int i ,a ,b;
+	int a[6], i, j;
 	srand(time(NULL));
-	cout << "數字:" ;
 	for(i=0;i<6;i++){
-		b=(rand()%49)+1;
-		printf(" %d",b);
+		a[i]=rand()%49+1;
+		for(j=6;j>=0;j--)
+		 if(a[i]==a[j]&&i!=j){
+		 	a[i]=rand()%49+1;
+		 }
+		printf("%d ",a[i]);
 	}
 	return 0;
 }
-//原解
