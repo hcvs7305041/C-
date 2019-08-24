68afd600a35c9f28f1b820160553ff7abe5c4e06
hcvs7305041 <48470597+hcvs7305041@users.noreply.github.com>
2019-08-25 00:00:48 +0800

    Create 希爾排序.cpp

---
+++ 希爾排序.cpp
@@ -0,0 +1,25 @@
+void shellsort (int a[],int n);
+void swap(int *a, int *b);
+
+
+
+void shellsort (int a[],int n){
+	n=n-1; //陣列0~9 
+	int i, j , gro=n/2; 
+	while(gro>0){  
+		for(i=0;(i+gro)<n;i++){  
+			if(a[i]>a[i+gro]){
+				swap(a[i],a[i+gro]);
+			}
+		}
+	//cout << gro <<  "  ";
+	gro=gro/2;
+	}	
+}
+
+void swap(int *a, int *b){
+	int c;
+	c=*a;
+	*a=*b;
+	*b=c;
+}
