26b5ebd527be8be12db837ef4121f15fdc31d440
hcvs7305041 <48470597+hcvs7305041@users.noreply.github.com>
2019-09-20 19:34:15 +0800

    Update TQC 501_610.cpp

--- TQC 501_610.cpp
+++ TQC 501_610.cpp
@@ -37,10 +37,10 @@ int main () {
  
     double area;
     struct circle c1={10, 10, 2.5};
-
-    area = c1.radius*c1.radius*3.14159;
  
-    printf("此圓的圓心為(%d, %d), 面積為%.2f\n", c1.x, c1.y, area);
+    area = c1.radius*c1.radius*3.14159; // c1.radius
+ 
+    printf("此圓的圓心為(%d, %d), 面積為%.2f\n", c1.x, c1.y, area); // c1.x c1.y
     return 0;
 }
 
